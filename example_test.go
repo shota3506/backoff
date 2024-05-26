a6c840da1c3843f89d320902261095f7f340a30e
shota3506 <s.shota.710.3506@gmail.com>
2024-05-26 17:13:22 +0900

    add example code

---
+++ example_test.go
@@ -0,0 +1,35 @@
+package backoff_test
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/shota3506/backoff"
+)
+
+func ExampleExponentialBackoff() {
+	do := func(i int) error {
+		log.Printf("do(%d): %s\n", i, time.Now())
+		if i < 2 {
+			return errors.New("error")
+		}
+		return nil
+	}
+
+	b, err := backoff.NewExponentialBackoff(backoff.ExponentialBackoffConfig{
+		InitialInterval:     100 * time.Millisecond,
+		RandomizationFactor: 0.5,
+		Multiplier:          2,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := range backoff.NewSleeper(b).Iter(5) {
+		if err := do(i); err == nil {
+			break
+		}
+		// retry after backoff
+	}
+}
